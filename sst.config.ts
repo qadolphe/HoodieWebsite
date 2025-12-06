99c2f5505e7835018b567ec73e3a348261b2b48c
qadolphe <117942328+qadolphe@users.noreply.github.com>
2025-12-05 21:53:45 -0500

    retry auto deploy

--- sst.config.ts
+++ sst.config.ts
@@ -32,12 +32,6 @@ export default $config({
           return { stage: "production" };
         }
       },
-      runner(input) {
-        return {
-          engine: "codebuild",
-          install: "npm install" 
-        };
-      }
     }
   }
 });
