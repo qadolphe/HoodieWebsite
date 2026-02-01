a1df733ca9349ea6fb1064e7ccaea865cbe0cf81
qadolphe <117942328+qadolphe@users.noreply.github.com>
2026-01-31 19:19:20 -0500

    feat: Update public SWATBLOC key for enhanced security

--- sst.config.ts
+++ sst.config.ts
@@ -15,7 +15,7 @@ export default $config({
     const site = new sst.aws.Nextjs("MyWeb", {
       link: [swatBlocSecret],
       environment: {
-        NEXT_PUBLIC_SWATBLOC_KEY: "pk_live_dnX7sfOjZoIhBrisMOQ5J9NFS6Ee1V2W",
+        NEXT_PUBLIC_SWATBLOC_KEY: "pk_live_Fu3btBtlMidrBfC2N2IAZnbm06OlOeOY",
         NEXT_PRIVATE_SWATBLOC_KEY: swatBlocSecret.value,
       }
     });
